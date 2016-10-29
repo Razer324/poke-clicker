771c674f3b585ede762e435941d15db5bcee5bb0
Joe Wolfgram <joseph.wolfgram@gmail.com>
2016-10-29 10:39:15 -0700

    Made changes to make the pokemon clicker work.

--- Projectawesome/scripts/pokemon.js
+++ Projectawesome/scripts/pokemon.js
@@ -2,19 +2,19 @@ var pokemonArray = ["images/aerodactyl.png","images/alakazam.png",
                     "images/gyarados.png","images/kabutops.png","images/kangaskhan.png",
                     "images/machamp.png","images/onix.png","images/rhydon.png","images/tauros.png","images/lugia.png"];
 var arrayItr = 0;
-var newHP = 10;
+var newHP = 2;
 var currentHP = newHP;
-var bossHP = (10 + newHP);
 
 document.getElementById("currentHP").textContent = currentHP;
-document.getElementById("image").addEventListener("click", function(){
+
+document.getElementById("image").addEventListener("copy", function(){
     console.log(currentHP);
     if (currentHP == 1) {
-        console.log("Goes in here");
         var isBoss = nextPokemon();
         if (isBoss == "notBoss") {
             console.log("I was told this is not a boss...");
             console.log(currentHP);
+            currentHP = newHP; // This is where the problem was... <--- I ADDED THIS <3
         } else {
             if (isBoss == "defeatedBoss") {
                 console.log("Boss defeated!");
@@ -29,19 +29,19 @@ document.getElementById("image").addEventListener("click", function(){
 
 function nextPokemon() {
     arrayItr += 1;
-    newHP += 1;
-    if (arrayItr >= 2) {
+    //newHP += 1;
+    if (arrayItr == 10) {
         console.log("Defeated Boss!");
         currentHP = newHP;
         arrayItr = 0;
         document.getElementById("image").setAttribute("src", pokemonArray[arrayItr]);
         return "defeatedBoss";
     } else {
-        if (arrayItr == 1) {
+        if (arrayItr == 9) {
             console.log("starting boss...");
-            currentHP = bossHP;
-            console.log(bossHP);
-            document.getElementById("image").setAttribute("src", pokemonArray[arrayItr]);
+            document.getElementById("image").setAttribute("src", pokemonArray[arrayItr]); // working as intended...next array would be 4 (Kabuto)
+            currentHP = (10 + newHP);
+            document.getElementById("currentHP").textContent = currentHP;
             return "boss";
         }
     }
