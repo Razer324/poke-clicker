b4e4046b05e2887465d6432e1c79bf0cab9f5517
Jovaniph <jovan008@gmail.com>
2016-10-29 12:34:19 -0400

    Add files via upload

--- Projectawesome/scripts/pokemon.js
+++ Projectawesome/scripts/pokemon.js
@@ -1,7 +1,7 @@
 var pokemonArray = ["images/aerodactyl.png","images/alakazam.png",
                     "images/gyarados.png","images/kabutops.png","images/kangaskhan.png",
                     "images/machamp.png","images/onix.png","images/rhydon.png","images/tauros.png","images/lugia.png"];
-var arrayItr = 1;
+var arrayItr = 0;
 var newHP = 10;
 var currentHP = newHP;
 var bossHP = (10 + newHP);
@@ -10,11 +10,10 @@ document.getElementById("currentHP").textContent = currentHP;
 document.getElementById("image").addEventListener("click", function(){
     console.log(currentHP);
     if (currentHP == 1) {
+        console.log("Goes in here");
         var isBoss = nextPokemon();
         if (isBoss == "notBoss") {
             console.log("I was told this is not a boss...");
-            newHP += 1;
-            currentHP = newHP;
             console.log(currentHP);
         } else {
             if (isBoss == "defeatedBoss") {
@@ -29,20 +28,23 @@ document.getElementById("image").addEventListener("click", function(){
 });
 
 function nextPokemon() {
-    document.getElementById("image").setAttribute("src", pokemonArray[arrayItr]);
     arrayItr += 1;
+    newHP += 1;
     if (arrayItr >= 2) {
         console.log("Defeated Boss!");
+        currentHP = newHP;
         arrayItr = 0;
+        document.getElementById("image").setAttribute("src", pokemonArray[arrayItr]);
         return "defeatedBoss";
     } else {
         if (arrayItr == 1) {
             console.log("starting boss...");
             currentHP = bossHP;
             console.log(bossHP);
+            document.getElementById("image").setAttribute("src", pokemonArray[arrayItr]);
             return "boss";
         }
     }
-
+    document.getElementById("image").setAttribute("src", pokemonArray[arrayItr]);
     return "notBoss";
 }
