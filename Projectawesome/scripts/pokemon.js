a216a98e02445daeab5580a1880e8c697f88c1be
Joe Wolfgram <joseph.wolfgram@gmail.com>
2016-10-29 12:06:55 -0700

    We made changes to add a score and starting to implement upgrades.

--- Projectawesome/scripts/pokemon.js
+++ Projectawesome/scripts/pokemon.js
@@ -4,27 +4,17 @@ var pokemonArray = ["images/aerodactyl.png","images/alakazam.png",
 var arrayItr = 0;
 var newHP = 2;
 var currentHP = newHP;
+var score = 0;
+var upgraded = false;
 
 document.getElementById("currentHP").textContent = currentHP;
 
-document.getElementById("image").addEventListener("copy", function(){
-    console.log(currentHP);
-    if (currentHP == 1) {
-        var isBoss = nextPokemon();
-        if (isBoss == "notBoss") {
-            console.log("I was told this is not a boss...");
-            console.log(currentHP);
-            currentHP = newHP; // This is where the problem was... <--- I ADDED THIS <3
-        } else {
-            if (isBoss == "defeatedBoss") {
-                console.log("Boss defeated!");
-                currentHP = newHP;
-            }
-        }
-    } else {
-        currentHP -= 1;
-    }
-    document.getElementById("currentHP").textContent = currentHP;
+document.getElementById("image").addEventListener("click", function(){
+    actionMadeToPokemon();
+});
+
+document.getElementById("upgrade").addEventListener("click", function(){
+    upgraded = true;
 });
 
 function nextPokemon() {
@@ -39,12 +29,60 @@ function nextPokemon() {
     } else {
         if (arrayItr == 9) {
             console.log("starting boss...");
+            addScore(10);
             document.getElementById("image").setAttribute("src", pokemonArray[arrayItr]); // working as intended...next array would be 4 (Kabuto)
             currentHP = (10 + newHP);
             document.getElementById("currentHP").textContent = currentHP;
             return "boss";
         }
     }
-    document.getElementById("image").setAttribute("src", pokemonArray[arrayItr]);
+    if (currentHP == 0) {
+        actionMadeToPokemon();
+    } else {
+        document.getElementById("image").setAttribute("src", pokemonArray[arrayItr]);
+    }
     return "notBoss";
+}
+
+function addScore(score2) {
+    score += score2;
+    document.getElementById('score').textContent = score;
+}
+
+function upgrades(argument) {
+        
+}
+
+function attackPokemon() {
+    if (upgraded) {
+        currentHP -= 2;
+    } else {
+        currentHP -= 1;
+    }
+}
+
+function actionMadeToPokemon() {
+        console.log(currentHP);
+    if (currentHP <= 1) {
+        var isBoss = nextPokemon();
+        if (isBoss == "notBoss") {
+            console.log("I was told this is not a boss...");
+            console.log(currentHP);
+            currentHP = newHP;
+            addScore(10);
+        } else {
+            if (isBoss == "defeatedBoss") {
+                console.log("Boss defeated!");
+                currentHP = newHP;
+                addScore(110);
+            }
+        }
+    } else {
+        attackPokemon();
+        if (currentHP <= 0) {
+            console.log(currentHP);
+            nextPokemon();
+        }
+    }
+    document.getElementById("currentHP").textContent = currentHP;
 }
